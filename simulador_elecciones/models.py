a5d2199713e7eb49892f1fc5d12c54100add0466
Martín Gallardo <69550249+martingallardo23@users.noreply.github.com>
2023-08-08 21:13:14 -0300

    fix data

--- simulador_elecciones/models.py
+++ simulador_elecciones/models.py
@@ -3,12 +3,12 @@ from django.db.models import JSONField
 
 class UserInput(models.Model):
     data = JSONField(blank=True, null=True, default=dict)
-    random_id = models.CharField(max_length=10, blank=True, null=True)
-    winner = models.CharField(max_length=10, blank=True, null=True)
-    round = models.CharField(max_length=10, blank=True, null=True)
+    random_id = models.CharField(max_length=250, blank=True, null=True)
+    winner = models.CharField(max_length=250, blank=True, null=True)
+    round = models.CharField(max_length=250, blank=True, null=True)
     percentage = models.FloatField(blank=True, null=True)
-    loser = models.CharField(max_length=10, blank=True, null=True)
-    hash = models.CharField(max_length=64, blank=True, null=True)
+    loser = models.CharField(max_length=250, blank=True, null=True)
+    hash = models.CharField(max_length=250, blank=True, null=True)
 
     def __str__(self):
         return f"{self.id}"
